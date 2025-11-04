cc31a5916aa946bd5233af061043f695d34041b5
BleytMC <150685954+BleytMC@users.noreply.github.com>
2025-11-04 19:31:52 +0200

    finished

--- main/main.cpp
+++ main/main.cpp
@@ -5,23 +5,24 @@
 #include <algorithm>
 #include <iomanip>
 #include <numeric>
+#include <conio.h>
 using namespace std;
 
 
 class car {
 	string name;
 	int year;
-	double engineValue, price;
+	double engineVolume, price;
 public:
-	car(string name, int year, double engineValue, double price) {
+	car(string name, int year, double engineVolume, double price) {
 		this->name = name;
 		this->year = year;
-		this->engineValue = engineValue;
+		this->engineVolume = engineVolume;
 		this->price = price;
 	}
 
 	void print() {
-		cout << "Car: " << name << "\n\tMade in " << year << "\n\tEngine value: " << engineValue << "L\n\tPrice: " << price << "$\n";
+		cout << "Car: " << name << "\n\tMade in " << year << "\n\tEngine volume: " << engineVolume << "L\n\tPrice: " << price << "$\n";
 	}
 
 	string getName() const {
@@ -32,8 +33,8 @@ public:
 		return year;
 	}
 
-	double getEngineValue() const {
-		return engineValue;
+	double getEngineVolume() const {
+		return engineVolume;
 	}
 
 	double getPrice() const {
@@ -56,6 +57,10 @@ public:
 		for (car c : cars) c.print();
 	}
 
+	bool isEmpty() {
+		return cars.size() == 0;
+	}
+
 	void addCar(car c) {
 		cars.push_back(c);
 	}
@@ -72,7 +77,7 @@ public:
 
 	dealership findByYear(int year) {
 		vector<car> result;
-		copy_if(cars.begin(), cars.end(), back_inserter(result), [year](const car& c) {return c.getYear() > year; });
+		copy_if(cars.begin(), cars.end(), back_inserter(result), [year](const car& c) {return c.getYear() >= year; });
 		return dealership(result);
 	}
 
@@ -106,14 +111,33 @@ public:
 		sort(cars.begin(), cars.end(), [](car a, car b) {return a.getYear() > b.getYear(); });
 	}
 
-	void sortByEngineValueToTop() {
-		sort(cars.begin(), cars.end(), [](car a, car b) {return a.getEngineValue() < b.getEngineValue(); });
+	void sortByEngineVolumeToTop() {
+		sort(cars.begin(), cars.end(), [](car a, car b) {return a.getEngineVolume() < b.getEngineVolume(); });
 	}
 
-	void sortByEngineValueToLow() {
-		sort(cars.begin(), cars.end(), [](car a, car b) {return a.getEngineValue() > b.getEngineValue(); });
+	void sortByEngineVolumeToLow() {
+		sort(cars.begin(), cars.end(), [](car a, car b) {return a.getEngineVolume() > b.getEngineVolume(); });
 	}
 };
+
+
+void menu() {
+	cout << "0 - Exit" << endl;
+	cout << "1 - Add car" << endl;
+	cout << "2 - Remove car" << endl;
+	cout << "3 - Show all cars" << endl;
+	cout << "4 - Search by name" << endl;
+	cout << "5 - Search by year" << endl;
+	cout << "6 - Search by price" << endl;
+	cout << "7 - Search the cheapest" << endl;
+	cout << "8 - Search the most expensive" << endl;
+	cout << "9 - Sort by price from the cheapest" << endl;
+	cout << "10 - Sort by price from the most expensive" << endl;
+	cout << "11 - Sort by year from the oldest" << endl;
+	cout << "12 - Sort by year from the newest" << endl;
+	cout << "13 - Sort by engine volume from the smallest" << endl;
+	cout << "14 - Sort by engine volume from the biggest" << endl;
+}
 
 
 
@@ -144,6 +168,87 @@ int main() {
 	d.sortByYearToTop();
 
 	d.print();
+
+
+	int choice, year, a, b;
+	string name;
+	double engineVolume, price;
+
+	do {
+		system("cls");
+		menu();
+		cin >> choice;
+		cout << endl;
+		switch (choice) {
+			case 1:
+				cout << "Enter car name: ";
+				cin >> name;
+				cout << "Enter year: ";
+				cin >> year;
+				cout << "Enter engine volume: ";
+				cin >> engineVolume;
+				cout << "Enter price: ";
+				cin >> price;
+				d.addCar(car(name, year, engineVolume, price));
+				break;
+			case 2:
+				cout << "Enter car name: ";
+				d.removeCar(name);
+				break;
+			case 3:
+				system("cls");
+				d.print();
+				break;
+			case 4:
+				cout << "Enter name: ";
+				cin >> name;
+				system("cls");
+				if (d.findByName(name).isEmpty()) cout << "Nothing found";
+				else d.findByName(name).print();
+				break;
+			case 5:
+				cout << "Enter year: ";
+				cin >> year;
+				system("cls");
+				if (d.findByYear(year).isEmpty()) cout << "Nothing found";
+				else d.findByYear(year).print();
+				break;
+			case 6:
+				cout << "Enter bottom price barrier: ";
+				cin >> a;
+				cout << "Enter top price barrier: ";
+				cin >> b;
+				system("cls");
+				if (d.findByPrice(a, b).isEmpty()) cout << "Nothing found";
+				else d.findByPrice(a, b).print();
+				break;
+			case 7:
+				d.findWithLowestPrice().print();
+				break;
+			case 8:
+				d.findWithHighestPrice().print();
+				break;
+			case 9:
+				d.sortByPriceToTop();
+				break;
+			case 10:
+				d.sortByPriceToLow();
+				break;
+			case 11:
+				d.sortByYearToTop();
+				break;
+			case 12:
+				d.sortByYearToLow();
+				break;
+			case 13:
+				d.sortByEngineVolumeToTop();
+				break;
+			case 14:
+				d.sortByEngineVolumeToLow();
+				break;
+		}
+		if (choice) _getch();
+	} while (choice);
 
 
 
